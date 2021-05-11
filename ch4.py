7e1bb70aee87ab94f3991ee99eeb8a33997631f7
Darren <darrenzhang200027@gmail.com>
2021-05-11 14:33:09 -0400

    added bit tricks

--- ch4.py
+++ ch4.py
@@ -10,7 +10,8 @@ def parityCheck(n):
         n >>= 1
     return 0 if even else 1
 
-def parityCheck2(n):
+# O(n) time, where n is the number of bits O(1) space
+def parityCheck2(n): # input = binary string
     bin_n = int(n, 2)
     parity = 0
     while bin_n:
@@ -20,4 +21,27 @@ def parityCheck2(n):
         bin_n >>= 1
     return parity
 
-print(parityCheck2('1011'))
+# print(parityCheck2('1011'))
+
+'''
+How can we do better? Right now the overhead is having to check every bit. We could improve this by only 
+counting the 1's bits.
+
+We can do n&(n-1) to get rid of the last 1 bit.
+That would make the time complexity O(k) where k is the number of 1 bits.
+
+00101100 n
+00101011 n - 1
+00101000 n & n - 1 -> here, we dropped the least significant 1 bit.
+
+'''
+
+def parityCheck3(n):
+    bin_n = int(n, 2)
+    parity = 0
+    while bin_n:
+        bin_n = bin_n & (bin_n - 1) # drop least significant 1 bit
+        parity = 1 - parity # toggles between 1 and 0
+    return parity
+
+# print(parityCheck3('10111'))
