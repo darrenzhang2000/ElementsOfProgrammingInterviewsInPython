f1d720a082137749197fbef8c282b18266247430
Darren <darrenzhang200027@gmail.com>
2021-05-28 10:24:21 -0400

    added epi soln for prime

--- ch5/5.9.enumeratePrimesToN.py
+++ ch5/5.9.enumeratePrimesToN.py
@@ -4,9 +4,9 @@ find all primes from 1 to n
 
 def enumeratePrimesToN(n):
     primes = []
-    for i in range(2, n + 1):
+    for i in range(2, n + 1): # O(n^2)
         isPrime = True
-        for j in range(2, i):
+        for j in range(2, int(math.sqrt(i))):
             if i % j == 0:
                 isPrime = False
                 break
@@ -18,23 +18,40 @@ def enumeratePrimesToN(n):
 
 import math
 def enumeratePrimesToN2(n):
-    s = set(i for i in range(2, n + 1))
-    for i in range(2, int(math.sqrt(n))):
-        if isPrime(i):
-            removeMultiplesFromSet(i, s, n)
+    s = set(i for i in range(2, n + 1)) 
+    for i in range(2, int(math.sqrt(n))): # O(sqrt(n)) time
+        if isPrime(i): # O(sqrt(n)) since we're only checking up to sqrt(n)
+            removeMultiplesFromSet(i, s, n) # remove at most n elements from set
     return list(s)
 
 def isPrime(i):
-    for j in range(2, i):
+    for j in range(2, int(math.sqrt(i))):
         if i % j == 0:
             return False
     return True
 
 def removeMultiplesFromSet(i, s, n):
-    multiplicity = 2
-    while multiplicity * i <= n:
-        if multiplicity * i in s:
-            s.remove(multiplicity * i)
-        multiplicity += 1
+    for p in range(2*i, n + 1, i):
+        if p in s:
+            s.remove(p)
 
-print(enumeratePrimesToN2(100))
+#print(enumeratePrimesToN2(100))
+
+'''
+epi soln
+'''
+def generatePrimes(n):
+    if n < 2:
+        return []
+    size = (n - 3) // 2 + 1
+    primes = [2]
+    isPrime = [True] * size
+    for i in range(size):
+        if isPrime[i]:
+            p = i * 2 + 3
+            primes.append(p)
+            for j in range(p*p, size, p):
+                isPrime[j] = False
+    return primes
+
+print(generatePrimes(100))
