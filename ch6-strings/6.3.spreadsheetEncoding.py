b5f3212cd5b2c06c44d4862c01cf771717a840c6
Darren <darrenzhang200027@gmail.com>
2021-06-05 11:59:08 -0400

    added reduce soln to spreadsheet encoding

--- ch6-strings/6.3.spreadsheetEncoding.py
+++ ch6-strings/6.3.spreadsheetEncoding.py
@@ -3,6 +3,8 @@ spreadsheets are made of letters A to Z. Compute the integer equivalent
 
 AA -> 1*26^1 + 1*26^0
 ZZ -> 26*26^1 + 26^26^0
+
+the reduce implemention would work by multiplying the accumulator by 26 and adding the int representation of the current char
 '''
 
 def computeSpreadsheetEncoding(w):
@@ -17,3 +19,17 @@ def computeSpreadsheetEncoding(w):
 print(computeSpreadsheetEncoding('AA'))
 print(computeSpreadsheetEncoding('ZZ'))
 print(computeSpreadsheetEncoding('A'))
+
+def ss_decode_col_id(w: str) -> int:
+    letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    n = 0
+    for i in range(len(w)):
+        cIdx = letters.index(w[i])
+        n = n * 26 + cIdx + 1
+    return n
+
+
+from functools import reduce
+def ss_decode_col_id2(w: str) -> int:
+    return reduce(lambda acc, c: 26 * acc + ord(c) - ord('A') + 1 , w, 0)
+
